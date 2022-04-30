40ee28bf8ec26beb822f4de9657a434e0825e953
Oleh Tsiroh <oleh_tsiroh@epam.com>
2022-04-30 14:58:13 +0300

    feat: create project structure

---
+++ src/app/registration/registration.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { RegistrationRoutingModule } from './registration-routing.module';
+import { RegistrationComponent } from './registration.component';
+
+@NgModule({
+  imports: [RegistrationRoutingModule],
+  declarations: [RegistrationComponent]
+})
+export class RegistrationModule {}
